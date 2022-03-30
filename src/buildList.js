a1572549fb2708d4599fca792cf8d5c2d38e085b
barenfels <barenfels@gmail.com>
2022-03-30 14:22:21 +0000

    Feat: rinkeby wevmos, crn

--- src/buildList.js
+++ src/buildList.js
@@ -1,6 +1,7 @@
 const { version } = require("../package.json");
 const mainnet = require("./tokens/mainnet.json");
 const testnet = require("./tokens/testnet.json");
+const rinkeby = require("./tokens/rinkeby.json");
 
 module.exports = function buildList() {
   const parsed = version.split(".");
@@ -13,9 +14,10 @@ module.exports = function buildList() {
       patch: +parsed[2],
     },
     tags: {},
-    logoURI: "https://raw.githubusercontent.com/cronus-finance/interface/main/public/images/512x512_App_Icon.png",
+    logoURI:
+      "https://raw.githubusercontent.com/cronus-finance/interface/main/public/images/512x512_App_Icon.png",
     keywords: ["cronus", "default"],
-    tokens: [...mainnet, ...testnet]
+    tokens: [...mainnet, ...testnet, ...rinkeby]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
         if (t1.chainId === t2.chainId) {
